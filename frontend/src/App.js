6b018147bdf3240a389b2ac3cabe566ea800846d
Omar Abouelkheir <omar@abouelkheirnz.com>
2023-09-18 14:36:28 +1200

    Hero image

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,9 +6,15 @@ import MetroNavBar from "./components/navbar";
 
 function App() {
   return (
-    <div class="bg-black h-full">
-      <div class="mx-24 my-6">
+    <div class="relative">
+      <img class="absolute mix-blend-overlay" src='/images/hero.svg'/>
+      <div class="mx-24">
         <MetroNavBar/>
+        <div class="flex flex-col py-28">
+          <h1 class="text-white font-bold text-5xl ">Let us find the perfect Rental</h1>
+          <h1 class="text-white font-bold text-5xl">property for you</h1>
+          <h2 class="text-white text-lg pt-10">Premium Rentals Await: Begin Your Search Here.</h2>
+        </div>
       </div>
     </div>
   );
