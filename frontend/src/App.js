83279c85d4a0149607eca4d19e9e2985c555119c
Omar Abouelkheir <omar@abouelkheirnz.com>
2023-09-19 01:02:57 +1200

    Added video section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -33,6 +33,33 @@ function App() {
       <div class="flex justify-center py-8">
        <button class="bg-transparent hover:bg-indigo-400 text-indigo-400 font-semibold hover:text-white py-2 px-4 border border-indigo-400 hover:border-transparent rounded">View Our Latest Listings</button>
       </div>
+      <div class="mx-24 pt-36 pb-12">
+        <img class="border-b border-gray-500 pb-6" src="/images/adbanner.png"/>
+      </div>
+      <div class="flex justify-center">
+        <h4 class="text-gray-600 font-semibold">1000+ HAPPY CLIENTS WORLDWIDE</h4>
+      </div>
+      <div class="mx-24 pt-28">
+        <h6 class="text-indigo-400 text-sm font-bold">WORK WITH THE BEST TODAY</h6>
+        <h4 class="text-4xl font-bold pb-3">Your Property, Our Priority</h4>
+        <h5 class="text-gray-500 font-semibold">Elevate Your Investment With Our Management Expertise</h5>
+      </div>
+      <div class="flex mx-20">
+        <div class="w-full">
+          <img src="/images/video.png"/>
+        </div>
+        <div class="flex-col">
+          <div class="flex flex-wrap">
+            <img src="/images/brag2.png"/>
+            <img src="/images/brag3.png"/>
+            <img src="/images/brag4.png"/>
+            <img src="/images/brag1.png"/>
+          </div>
+          <div class="w-96">
+            <p class="text-xs ">We excel in property management tasks, going beyond the norm. Our services include expert staging to attract top-tier renters, rigorous tenant selection, and market-savvy management in Auckland. We guarantee high returns, peace of mind, and risk reduction. Our award-winning systems ensure excellence in every aspect. Contact our experienced Auckland team for top-notch property management.</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
